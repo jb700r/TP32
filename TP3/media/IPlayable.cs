a7cfdcba0781c865b76967804dce3fbb0e6d9ed0
jeffrey bilodeau <jeffreybilodeau5@gmail.com>
2024-05-02 11:14:53 -0400

    jeffrey

---
+++ TP3/media/IPlayable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3.media
+{
+    public interface IPlayable
+    {
+        void Play();
+        void Stop();
+    }
+}
---
+++ TP3/media/Media.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMPLib;
+
+namespace TP3.media
+{
+    public abstract class Media : IPlayable
+    {
+        private string title;
+        private int year;
+        private WindowsMediaPlayer player;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+        public WindowsMediaPlayer Player
+        {
+            get { return player; }
+            set { player = value; }
+        }
+
+        public Media(string title,int year) 
+        {
+            this.Title = title;
+            this.Year = year;
+        }
+
+        public void Play() { }
+        public void Stop() { }
+
+        public override bool Equals(object obj)
+        {
+            if ( ((Media)obj).Title == this.Title && ((Media)obj).Year == this.Year)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{this.Title} from ({this.Year})";
+        }
+    }
+}
